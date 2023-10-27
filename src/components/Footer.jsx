3f122c05f824b15810fb0e739599e6c2afcdf28a
ArtExpress <ixpressofficial@gmail.com>
2023-10-27 05:34:56 -0700

    meta tag update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <a href="/">
+        <img src="" className='' />
+      </a>
+    
+      <div>
+          <ul>
+              <a>
+                  <li>Home</li>
+              </a>
+              <a>
+                  <li>About</li>
+              </a>
+              <a>
+                  <li>Reservation</li>
+              </a>
+              <a>
+                  <li>Order Online</li>
+              </a>
+              <a>
+                  <li>Login</li>
+              </a>
+          </ul>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+        <a href="/">
+            <img src="" className='' />
+        </a>
+    
+        <div>
+            <ul>
+                <a>
+                    <li>Home</li>
+                </a>
+                <a>
+                    <li>About</li>
+                </a>
+                <a>
+                    <li>Reservation</li>
+                </a>
+                <a>
+                    <li>Order Online</li>
+                </a>
+                <a>
+                    <li>Login</li>
+                </a>
+            </ul>
+        </div>
+    </nav>>
+  )
+}
+
+export default Nav
